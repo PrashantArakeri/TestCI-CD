fb95e617c3d4af6734accbfeb09f8950bb12b71a
PrashantArakeri <87301755+PrashantArakeri@users.noreply.github.com>
2021-10-04 16:04:50 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,28 @@
+
+
+#!/bin/bash
+PACE_API_URL=https://pace.qentinel.com/pace/v4/builds
+PACE_API_KEY=ca3f9f15-2db7-499b-aaf6-e74c9022db3c
+# Start the build
+BUILD=$(curl -sS -d '{"key": "'${PACE_API_KEY}'", "inputParameters": [{"key": "BROWSER", "value": "firefox"}]}' -H "Content-Type: application/json" -X POST ${PACE_API_URL})
+echo "${BUILD}"
+BUILD_ID=$(echo "${BUILD}" | grep -Po '"id":\K[0-9]+')
+if [ -z "${BUILD_ID}" ]; then
+  exit 1
+fi
+echo -n "Executing tests "
+STATUS='"executing"'
+# Poll every 10 seconds until the build is finished
+while [ "${STATUS}" == '"executing"' ]; do
+  sleep 10
+  RESULTS=$(curl -sS ${PACE_API_URL}/${BUILD_ID}?key=${PACE_API_KEY})
+  STATUS=$(echo "${RESULTS}" | grep -Po '"status": *\K"[^"]*"' | head -1)
+  echo -n "."
+done
+echo " done!"
+FAILURES=$(echo ${RESULTS} | grep -Po '"failures":\K[0-9]+')
+if(failures>=2)
+exit -1
+LOG_REPORT_URL=$(echo "${RESULTS}" | grep -Po '"logReportUrl": *\K"[^"]*"')
+echo "Report URL: ${LOG_REPORT_URL}"
+
