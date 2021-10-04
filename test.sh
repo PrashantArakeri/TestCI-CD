b96e0b99ef3e497972c1d02fd5ed1a9d8c5231ac
PrashantArakeri <87301755+PrashantArakeri@users.noreply.github.com>
2021-10-04 16:55:43 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 PACE_API_URL=https://pace.qentinel.com/pace/v4/builds
 PACE_API_KEY=ca3f9f15-2db7-499b-aaf6-e74c9022db3c
 # Start the build
-BUILD=$(curl -sS -d '{"key": "'${PACE_API_KEY}'", "inputParameters": [{"key": "BROWSER", "value": "firefox"}]}' -H "Content-Type: application/json" -X POST ${PACE_API_URL})
+BUILD=$(curl -sS -d '{"key": "'${PACE_API_KEY}'", "inputParameters": [{"key": "BROWSER", "value": "chrome"}]}' -H "Content-Type: application/json" -X POST ${PACE_API_URL})
 echo "${BUILD}"
 BUILD_ID=$(echo "${BUILD}" | grep -Po '"id":\K[0-9]+')
 if [ -z "${BUILD_ID}" ]; then
